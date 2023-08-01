1fab2b939ccf79b087ad02583d7c5e3afb605f8b
Johann <folta@brandeis.edu>
2023-08-01 14:17:28 -0400

    got test app running on port 3000 (ERR_TLS_CERT_ALTNAME_INVALID npm i issue via library wifi indentified and bypassed via hotspot)

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send("You have arrived")  
-}
+app.get("/", (req, res) => {
+  res.send("You have arrived");
+});
 
 app.listen(3000, function () {
-  console.log("Listening on port 3000...")
-})
+  console.log("Listening on port 3000...");
+});
